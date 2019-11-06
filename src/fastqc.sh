f0d39dae8f6aa0e1f0c983730130d2236b5b3b88
Tkastylevsky <timothee.kastylevsky@ens-lyon.fr>
2019-11-06 11:07:53 +0000

    multiqc + fastqc

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -4,5 +4,21 @@
 
 # Create a folder for the fastqc
 
+# Create a working directory:
+data="/home/rstudio/disk/tp/data"
+cd $data
+
+# Create a directory where the data will be downloaded
+mkdir -p fastqc_data
+
+cd  /home/rstudio/disk/tp/data
 # Pour chaque fastq 
-fastqc -h
+
+li=`ls sra_data/*.fastq`
+
+for i in $li
+do
+echo $i 
+
+fastqc $i -O /home/rstudio/disk/tp/data/fastqc_data
+done
--- src/multiqc.sh
+++ src/multiqc.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Create a working directory:
+data="/home/rstudio/disk/tp/data"
+cd $data
 
-multiqc -h 
+# Create a directory where the data will be downloaded
+mkdir -p multiqc_data
+
+cd  /home/rstudio/disk/tp/data
+
+multiqc fastqc_data -o /home/rstudio/disk/tp/data/multiqc_data
 
 
 
