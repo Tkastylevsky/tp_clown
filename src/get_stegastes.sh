5b961eea5b03fa9a29906987198c9da7c32df8a4
Tkastylevsky <timothee.kastylevsky@ens-lyon.fr>
2019-11-06 14:55:53 +0000

    transdec get_stegastes

--- src/get_stegastes.sh
+++ src/get_stegastes.sh
@@ -1,8 +1,11 @@
-wget -O stegastes.fa 'http://ensembl.org/biomart/...
-                        
-        <Dataset name = "..." interface = "default" >
-                <Attribute name = "..." />
-                <Attribute name = "..." />
-                <Attribute name = "..." />
-        </Dataset>
+cd /home/rstudio/disk/tp/data/transdec_data
+wget -O stegastes.fa 'http://ensembl.org/biomart/martservice?query=
+
+<Query  virtualSchemaName = "default" formatter = "FASTA" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.6" >
+				
+	<Dataset name = "spartitus_gene_ensembl" interface = "default" >
+		<Attribute name = "coding" />
+		<Attribute name = "external_gene_name" />
+		<Attribute name = "ensembl_gene_id" />
+	</Dataset>
 </Query>'
--- src/transdec.sh
+++ src/transdec.sh
@@ -3,5 +3,18 @@
 # launch Transdecoder
 # work in 2 or 3 steps (the second is facultative)
 # Step 1: extract the long open reading frames
-TransDecoder.LongOrfs 
+
+data="/home/rstudio/disk/test2"
+cd $data
+
+# Create a directory where the data will be downloaded
+mkdir -p Transdec_data
+
+cp /home/disk/test2/Trinity_data/trinity.fasta trinity.fasta
+cd Trinity_data
+TransDecoder.LongOrfs -t Trinity.fasta
+
+
+
+
 TransDecoder.Predict 
